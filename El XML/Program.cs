e45c746d288f78231277d08dd4d9f715370494b9
Brayan Jose Pineda Castillo <134087681+Brayan2055XD@users.noreply.github.com>
2023-10-19 02:04:12 -0400

    EL XML BLANCO
    Use el método XMLWhriter, aunque es molesto modificarlo.

---
+++ El XML/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Crear el documento XML
+        XmlWriterSettings settings = new XmlWriterSettings();
+        settings.Indent = true;
+
+        XmlWriter writer = XmlWriter.Create("SciFiMovies.xml", settings);
+        writer.WriteStartDocument();
+        writer.WriteStartElement("Movies");
+
+        writer.WriteStartElement("Movie");
+        writer.WriteElementString("Title", "Star Wars");
+        writer.WriteElementString("Director", "George Lucas");
+        writer.WriteElementString("ReleaseYear", "1977");
+        writer.WriteEndElement();
+
+        writer.WriteStartElement("Movie");
+        writer.WriteElementString("Title", "The Matrix");
+        writer.WriteElementString("Director", "The Wachowskis");
+        writer.WriteElementString("ReleaseYear", "1999");
+        writer.WriteEndElement();
+
+        writer.WriteEndElement();
+        writer.WriteEndDocument();
+
+        writer.Flush();
+        writer.Close();
+
+        // Cargar el documento XML
+        XDocument xmlDoc = XDocument.Load("SciFiMovies.xml");
+
+        // Consulta LINQ para obtener los títulos de todas las películas
+        var movieTitles = from movie in xmlDoc.Descendants("Movie")
+                          select movie.Element("Title").Value;
+
+        // Imprimir los títulos
+        Console.WriteLine("Títulos de las películas:");
+        foreach (var title in movieTitles)
+        {
+            Console.WriteLine(title);
+        }
+    }
+}
